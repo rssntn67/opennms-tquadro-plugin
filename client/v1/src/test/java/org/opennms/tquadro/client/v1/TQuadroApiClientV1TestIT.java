75efa8dc66ca3b3ecd78b4fba48a5942cef75a68
Antonio <rssntn67@yahoo.it>
2023-11-03 15:06:27 +0100

    Added Provider Test and reverted jackson dependency

---
+++ client/v1/src/test/java/org/opennms/tquadro/client/v1/TQuadroApiClientV1TestIT.java
@@ -0,0 +1,33 @@
+package org.opennms.tquadro.client.v1;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opennms.tquadro.client.api.ApiClientCredentials;
+import org.opennms.tquadro.client.v1.api.AccountApi;
+import org.opennms.tquadro.client.v1.api.OpenNmsApi;
+import org.opennms.tquadro.client.v1.handler.ApiException;
+import org.opennms.tquadro.client.v1.model.AssetOpenNMS;
+import org.opennms.tquadro.client.v1.model.AuthenticationUser;
+import org.opennms.tquadro.client.v1.model.LoginAPIViewModel;
+
+public class TQuadroApiClientV1TestIT {
+
+    @Test
+    public void validateTest() {
+        V1ApiClientProvider provider = new V1ApiClientProvider();
+        provider.validate(getCredential());
+    }
+
+    private ApiClientCredentials getCredential() {
+        return ApiClientCredentials.builder()
+                .withTQuadroUrl("https://tquadro.arsinfo.it:9443")
+                .withUsername(System.getenv("TQ_USER"))
+                .withPassword(System.getenv("TQ_PASS"))
+                .withIgnoreSslCertificateValidation(true)
+                .build();
+    }
+
+}
