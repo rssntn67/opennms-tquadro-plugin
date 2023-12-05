dcf1d867d20b3b051620fc9b1a8f21bb2843182b
Antonio <rssntn67@yahoo.it>
2023-12-05 12:35:10 +0100

    Added apiClient to handler

---
+++ client/v1/src/main/java/org/opennms/tquadro/client/v1/handler/JSON.java
@@ -0,0 +1,40 @@
+package org.opennms.tquadro.client.v1.handler;
+
+import java.text.DateFormat;
+
+import javax.ws.rs.ext.ContextResolver;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JSON implements ContextResolver<ObjectMapper> {
+  private final ObjectMapper mapper;
+
+  public JSON() {
+    mapper = new ObjectMapper();
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+    mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+    mapper.setDateFormat(new RFC3339DateFormat());
+    mapper.registerModule(new JavaTimeModule());
+  }
+
+  /**
+   * Set the date format for JSON (de)serialization with Date properties.
+   * @param dateFormat Date format
+   */
+  public void setDateFormat(DateFormat dateFormat) {
+    mapper.setDateFormat(dateFormat);
+  }
+
+  @Override
+  public ObjectMapper getContext(Class<?> type) {
+    return mapper;
+  }
+}
---
+++ client/v1/src/main/java/org/opennms/tquadro/client/v1/handler/auth/OAuthFlow.java
@@ -0,0 +1,5 @@
+package org.opennms.tquadro.client.v1.handler.auth;
+
+public enum OAuthFlow {
+    accessCode, implicit, password, application
+}
