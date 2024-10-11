b318f02e33ad8c043f71f999fc853b3d35955d49
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2024-10-11 08:47:13 +0200

    Removed Lombok

--- plugin/src/main/java/org/opennms/tquadro/plugin/ConnectionDTO.java
+++ plugin/src/main/java/org/opennms/tquadro/plugin/ConnectionDTO.java
@@ -1,11 +1,8 @@
 package org.opennms.tquadro.plugin;
 
-import lombok.Data;
-
 /**
  *  DTO for connection information received by the API
  */
-@Data
 public class ConnectionDTO {
     private String alias;
     private String prismUrl;
@@ -15,4 +12,51 @@ public class ConnectionDTO {
     private Integer length;
     private Boolean ignoreSslCertificateValidation;
 
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getPrismUrl() {
+        return prismUrl;
+    }
+
+    public void setPrismUrl(String prismUrl) {
+        this.prismUrl = prismUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Boolean getIgnoreSslCertificateValidation() {
+        return ignoreSslCertificateValidation;
+    }
+
+    public void setIgnoreSslCertificateValidation(Boolean ignoreSslCertificateValidation) {
+        this.ignoreSslCertificateValidation = ignoreSslCertificateValidation;
+    }
 }
--- plugin/src/main/java/org/opennms/tquadro/plugin/ConnectionStateDTO.java
+++ plugin/src/main/java/org/opennms/tquadro/plugin/ConnectionStateDTO.java
@@ -1,10 +1,32 @@
 package org.opennms.tquadro.plugin;
 
-import lombok.Data;
 
-@Data
 public class ConnectionStateDTO {
     private String alias;
     private String prismUrl;
     private boolean valid;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getPrismUrl() {
+        return prismUrl;
+    }
+
+    public void setPrismUrl(String prismUrl) {
+        this.prismUrl = prismUrl;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
 }
